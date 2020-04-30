06af0756e48c51349d1281cb93bcee4c0633525f
jjlljj <logueja@gmail.com>
2020-04-30 12:08:43 -0600

    Initial Commit

---
+++ programmatic-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  programmatic-ios
+//
+//  Created by James on 4/30/20.
+//  Copyright © 2020 jjlljj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ programmatic-iosTests/programmatic_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  programmatic_iosTests.swift
+//  programmatic-iosTests
+//
+//  Created by James on 4/30/20.
+//  Copyright © 2020 jjlljj. All rights reserved.
+//
+
+import XCTest
+@testable import programmatic_ios
+
+class programmatic_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
