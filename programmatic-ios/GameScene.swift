7bad053ce8f83b96245e32dec940830e8fcefb22
jjlljj <logueja@gmail.com>
2020-05-15 18:05:43 -0600

    Add sidescrolling non-player sprite blocks

--- programmatic-ios/GameScene.swift
+++ programmatic-ios/GameScene.swift
@@ -9,22 +9,51 @@
 import SpriteKit
 
 class GameScene: SKScene {
-
+    
     let player = SKSpriteNode(color: UIColor.blue, size: CGSize(width:20, height:40))
-
+    
     override func didMove(to view: SKView) {
-      super.didMove(to:view)
-      backgroundColor = SKColor.white
-
-     
-      player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-      addChild(player)
-      print("SCENE DID MOVE")
-      print(size)
+        super.didMove(to:view)
+        backgroundColor = SKColor.white
+        
+        
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        addChild(player)
+        
+        run(SKAction.repeatForever(
+          SKAction.sequence([
+            SKAction.run(addMonster),
+            SKAction.wait(forDuration: 1.0)
+            ])
+        ))
     }
     
     override func sceneDidLoad() {
-      super.sceneDidLoad()
-      print("SCENE DID LOAD")
+        super.sceneDidLoad()
+        print("SCENE DID LOAD")
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 4294967296)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
+        let monster = SKSpriteNode(color: UIColor.red, size: CGSize(width:30, height:30))
+        
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        addChild(monster)
+        
+        let actualDuration = random(min: CGFloat(4.0), max: CGFloat(6.0))
+        
+        let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
 }
