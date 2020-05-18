a121841bb554e47afcf5265312f2e69e01ef5e99
jjlljj <logueja@gmail.com>
2020-05-18 16:13:18 -0600

    Add collision detection and object removal

--- spritekit-game-ios/GameScene.swift
+++ spritekit-game-ios/GameScene.swift
@@ -8,6 +8,13 @@
 
 import SpriteKit
 
+struct PhysicsCategory {
+  static let none      : UInt32 = 0
+  static let all       : UInt32 = UInt32.max
+  static let monster   : UInt32 = 0b1
+  static let projectile: UInt32 = 0b10
+}
+
 func +(left: CGPoint, right: CGPoint) -> CGPoint {
   return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
@@ -48,6 +55,8 @@ class GameScene: SKScene {
     super.didMove(to:view)
     backgroundColor = SKColor.white
     
+    physicsWorld.gravity = .zero
+    physicsWorld.contactDelegate = self
     
     player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
     addChild(player)
@@ -75,6 +84,11 @@ class GameScene: SKScene {
   
   func addMonster() {
     let monster = SKSpriteNode(color: UIColor.red, size: CGSize(width:30, height:30))
+    monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
+    monster.physicsBody?.isDynamic = true
+    monster.physicsBody?.categoryBitMask = PhysicsCategory.monster
+    monster.physicsBody?.contactTestBitMask = PhysicsCategory.projectile
+    monster.physicsBody?.collisionBitMask = PhysicsCategory.none
     
     //let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
     let actualX = random(min: monster.size.width/2, max: size.width - monster.size.width/2)
@@ -102,6 +116,12 @@ class GameScene: SKScene {
     let projectile = SKSpriteNode(color: UIColor.purple, size: CGSize(width:2, height:6))
     projectile.position = player.position
     
+    projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+    projectile.physicsBody?.isDynamic = true
+    projectile.physicsBody?.categoryBitMask = PhysicsCategory.projectile
+    projectile.physicsBody?.contactTestBitMask = PhysicsCategory.monster
+    projectile.physicsBody?.collisionBitMask = PhysicsCategory.none
+    projectile.physicsBody?.usesPreciseCollisionDetection = true
     
     let offset = touchLocation - projectile.position
     
@@ -117,5 +137,33 @@ class GameScene: SKScene {
     let actionMove = SKAction.move(to: realDestination, duration: 2.5)
     let actionMoveDone = SKAction.removeFromParent()
     projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
+  }
+  
+  func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
+    print("Hit")
+    projectile.removeFromParent()
+    monster.removeFromParent()
+  }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+  func didBegin(_ contact: SKPhysicsContact) {
+    var firstBody: SKPhysicsBody
+    var secondBody: SKPhysicsBody
+    if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+      firstBody = contact.bodyA
+      secondBody = contact.bodyB
+    } else {
+      firstBody = contact.bodyB
+      secondBody = contact.bodyA
+    }
+   
+    if ((firstBody.categoryBitMask & PhysicsCategory.monster != 0) &&
+        (secondBody.categoryBitMask & PhysicsCategory.projectile != 0)) {
+      if let monster = firstBody.node as? SKSpriteNode,
+        let projectile = secondBody.node as? SKSpriteNode {
+        projectileDidCollideWithMonster(projectile: projectile, monster: monster)
+      }
+    }
   }
 }
