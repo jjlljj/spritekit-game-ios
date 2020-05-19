5600ca94891f3d2eff051ff91a0a4afa7f78536c
jjlljj <logueja@gmail.com>
2020-05-19 11:07:09 -0600

    Add character assets for spaceship, asteroid

--- spritekit-game-ios/GameScene.swift
+++ spritekit-game-ios/GameScene.swift
@@ -11,7 +11,7 @@ import SpriteKit
 struct PhysicsCategory {
   static let none      : UInt32 = 0
   static let all       : UInt32 = UInt32.max
-  static let monster   : UInt32 = 0b1
+  static let asteroid   : UInt32 = 0b1
   static let projectile: UInt32 = 0b10
 }
 
@@ -49,7 +49,8 @@ extension CGPoint {
 
 class GameScene: SKScene {
   
-  let player = SKSpriteNode(color: UIColor.blue, size: CGSize(width:20, height:40))
+  //let player = SKSpriteNode(color: UIColor.blue, size: CGSize(width:20, height:40))
+  let player = SKSpriteNode(imageNamed: "spaceship.png")
   
   override func didMove(to view: SKView) {
     super.didMove(to:view)
@@ -63,7 +64,7 @@ class GameScene: SKScene {
     
     run(SKAction.repeatForever(
       SKAction.sequence([
-        SKAction.run(addMonster),
+        SKAction.run(addAsteroid),
         SKAction.wait(forDuration: 1.5)
       ])
     ))
@@ -82,28 +83,25 @@ class GameScene: SKScene {
     return random() * (max - min) + min
   }
   
-  func addMonster() {
-    let monster = SKSpriteNode(color: UIColor.red, size: CGSize(width:30, height:30))
-    monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
-    monster.physicsBody?.isDynamic = true
-    monster.physicsBody?.categoryBitMask = PhysicsCategory.monster
-    monster.physicsBody?.contactTestBitMask = PhysicsCategory.projectile
-    monster.physicsBody?.collisionBitMask = PhysicsCategory.none
+  func addAsteroid() {
+    let asteroid = SKSpriteNode(imageNamed: "asteroid.png")
+    asteroid.physicsBody = SKPhysicsBody(rectangleOf: asteroid.size)
+    asteroid.physicsBody?.isDynamic = true
+    asteroid.physicsBody?.categoryBitMask = PhysicsCategory.asteroid
+    asteroid.physicsBody?.contactTestBitMask = PhysicsCategory.projectile
+    asteroid.physicsBody?.collisionBitMask = PhysicsCategory.none
     
-    //let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
-    let actualX = random(min: monster.size.width/2, max: size.width - monster.size.width/2)
+    let actualX = random(min: asteroid.size.width/2, max: size.width - asteroid.size.width/2)
     
-    //monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
-    monster.position = CGPoint(x: actualX, y: size.height + monster.size.height/2)
+    asteroid.position = CGPoint(x: actualX, y: size.height + asteroid.size.height/2)
     
-    addChild(monster)
+    addChild(asteroid)
     
     let actualDuration = random(min: CGFloat(5.0), max: CGFloat(8.0))
     
-    //let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
-    let actionMove = SKAction.move(to: CGPoint(x: actualX, y: -monster.size.height/2), duration: TimeInterval(actualDuration))
+    let actionMove = SKAction.move(to: CGPoint(x: actualX, y: -asteroid.size.height/2), duration: TimeInterval(actualDuration))
     let actionMoveDone = SKAction.removeFromParent()
-    monster.run(SKAction.sequence([actionMove, actionMoveDone]))
+    asteroid.run(SKAction.sequence([actionMove, actionMoveDone]))
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -113,19 +111,19 @@ class GameScene: SKScene {
     
     let touchLocation = touch.location(in: self)
     
-    let projectile = SKSpriteNode(color: UIColor.purple, size: CGSize(width:2, height:6))
+    let projectile = SKSpriteNode(color: UIColor.red, size: CGSize(width:3, height:12))
     projectile.position = player.position
     
     projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
     projectile.physicsBody?.isDynamic = true
     projectile.physicsBody?.categoryBitMask = PhysicsCategory.projectile
-    projectile.physicsBody?.contactTestBitMask = PhysicsCategory.monster
+    projectile.physicsBody?.contactTestBitMask = PhysicsCategory.asteroid
     projectile.physicsBody?.collisionBitMask = PhysicsCategory.none
     projectile.physicsBody?.usesPreciseCollisionDetection = true
     
     let offset = touchLocation - projectile.position
     
-    if offset.y < 100 { return }
+    if offset.y < 60 { return }
     
     addChild(projectile)
     
@@ -139,10 +137,10 @@ class GameScene: SKScene {
     projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
   }
   
-  func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
+  func projectileDidCollideWithAsteroid(projectile: SKSpriteNode, asteroid: SKSpriteNode) {
     print("Hit")
     projectile.removeFromParent()
-    monster.removeFromParent()
+    asteroid.removeFromParent()
   }
 }
 
@@ -158,11 +156,11 @@ extension GameScene: SKPhysicsContactDelegate {
       secondBody = contact.bodyA
     }
    
-    if ((firstBody.categoryBitMask & PhysicsCategory.monster != 0) &&
+    if ((firstBody.categoryBitMask & PhysicsCategory.asteroid != 0) &&
         (secondBody.categoryBitMask & PhysicsCategory.projectile != 0)) {
-      if let monster = firstBody.node as? SKSpriteNode,
+      if let asteroid = firstBody.node as? SKSpriteNode,
         let projectile = secondBody.node as? SKSpriteNode {
-        projectileDidCollideWithMonster(projectile: projectile, monster: monster)
+        projectileDidCollideWithAsteroid(projectile: projectile, asteroid: asteroid)
       }
     }
   }
